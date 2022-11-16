517f71a70a0e4852c8a0af8190ad575a9e204aec
Dmitriy <zdv9997@mail.ru>
2022-11-16 11:55:04 +0200

    находим максимальное число из трех введенных

---
+++ zad4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число C: ");
+int C = Convert.ToInt32(Console.ReadLine());
+int max = A;
+if (B > max) max = B;
+if (C > max) max = C;
+Console.WriteLine($"Максимальное число равно {max}");
