892b867666fa7f44d1f4333119e47bf4bcb60b46
Dmitriy <zdv9997@mail.ru>
2022-11-16 12:40:08 +0200

    проверяем число на четность

---
+++ zad6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+float A = Convert.ToInt32(Console.ReadLine());
+if (A % 2 == 0)
+{
+    Console.WriteLine("Это четное число");
+}
+else
+{
+    Console.WriteLine("Это не четное число");
+}
