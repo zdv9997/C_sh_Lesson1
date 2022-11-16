1dfe1ed2c26477bdf3eba48ac2ecf31f68dca380
Dmitriy <zdv9997@mail.ru>
2022-11-16 11:44:31 +0200

    вводим и сравниваем числа

---
+++ zad2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+if (A > B)
+{
+    Console.WriteLine($"Число A больше B. A={A} B={B}");
+}
+else
+{
+    Console.WriteLine($"Число B больше A. A={A} B={B}");
+}
